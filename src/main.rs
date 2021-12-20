5f24b0d4a04ac6d3f36cc720d89f92fe3e3e951d
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2021-12-20 20:49:09 +0200

    Renaming the bin

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let listener = std::net::TcpListener::bind("0.0.0.0:8083").expect("Failed to bind to port 80");
-    api::run(listener)?.await
+    api_actix_web::run(listener)?.await
 }
